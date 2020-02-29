9fa1c2f2467df3e69aa6501f036d4fc554fc74aa
sh0rez <me@shorez.de>
2020-02-29 21:37:49 +0100

    style: sort imports

--- pkg/kubernetes/client/context.go
+++ pkg/kubernetes/client/context.go
@@ -9,9 +9,10 @@ import (
 	"os/exec"
 	"strings"
 
-	"github.com/grafana/tanka/pkg/defers"
 	"github.com/stretchr/objx"
 	funk "github.com/thoas/go-funk"
+
+	"github.com/grafana/tanka/pkg/defers"
 )
 
 // findContext returns a valid context from $KUBECONFIG that uses the given
